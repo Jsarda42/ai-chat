e085cd3f624485b29813fc193cecc1191edfc2ab
Julien Sarda <juliensarda@Juliens-MacBook-Air.local>
2026-01-18 13:00:27 +0900

    fix version for next to remove turbo

--- portfolio/src/app/layout.tsx
+++ portfolio/src/app/layout.tsx
@@ -1,14 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Inter } from "next/font/google";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const inter = Inter({
   subsets: ["latin"],
 });
 
@@ -25,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.className} bg-white text-black dark:bg-black dark:text-white transition-colors duration-500`}
       >
         {children}
       </body>
